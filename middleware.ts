1bfd0295e3e7baa71631db41fdaab20709d4e1c5
Winfield Wellington <winfield.wellington@gmail.com>
2026-02-25 08:28:45 -0500

    Remove middleware to stop MIDDLEWARE_INVOCATION_FAILED

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-export async function middleware(request: NextRequest) {
-  return NextResponse.next({ request });
-}
-
-export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|manifest.webmanifest|sw.js).*)"]
-};
